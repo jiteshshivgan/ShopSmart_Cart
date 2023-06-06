f8f3be25a6b8ede57aaa5280809a22d92916f15c
JITESH KUMAR SHIVGAN <jiteshshivgan5@gmail.com>
2023-06-06 19:42:43 +0530

    delete product

--- src/Cart.js
+++ src/Cart.js
@@ -12,25 +12,61 @@ class Cart extends React.Component{
             price: "10000",
             title: "Phone",
             qty: 1,
-            img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg"
+            img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg",
+            id: 1
             
             },
             {
                 price: "1000",
                 title: "Watch",
                 qty: 1,
-                img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg"
-                
+                img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg",
+                id: 2
             },
             {
                     price: "9000",
                     title: "Laptop",
                     qty: 1,
-                    img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg"
-                    
+                    img: "https://cdn.pixabay.com/photo/2014/09/24/14/29/macbook-459196_1280.jpg",
+                    id: 3
             }
     ]}
     }
+    increaseQuantity =(product) =>{
+        
+         console.log("Hey increase the qty of watch", product);
+         const {products} = this.state;
+         const index=products.indexOf(product);
+         products[index].qty=products[index].qty+1;
+         this.setState({
+            products: products
+         })
+    }
+
+    decreaseQuantity=(product)=>{
+       
+        const {products} = this.state;
+        const index=products.indexOf(product);
+        if(products[index].qty>0)
+             products[index].qty=products[index].qty-1;
+        this.setState({
+           products: products
+        })
+    }
+
+    deleteQuantity=(product)=>{
+        const {products} =this.state;
+        const id=product.id;
+        const items=products.filter((item)=>{
+              return item.id!==product.id; 
+        })
+        this.setState({
+            products: items
+        })
+    }
+    
+    
+    
     render(){
         const {products}=this.state;
         return(
@@ -38,7 +74,7 @@ class Cart extends React.Component{
             <div className='cart'>
                 
                 {products.map((product, index)=>{
-                    return <CartItem product={product} key={index}/>
+                    return <CartItem product={product} key={index} onIncreaseQuantity={this.increaseQuantity} onDecreaseQuantity={this.decreaseQuantity} onDeleteQuantity={this.deleteQuantity}/>
                 })}
                 
                 
--- src/CartItem.js
+++ src/CartItem.js
@@ -15,39 +15,37 @@ class CartItem extends React.Component{
     // }
     
     //Arrow function will automatically bind the function with the instance of CartItem class
-   increaseQuantity =() =>{
-        // console.log("this.state", this.state);
-        // this.state.qty+=1;
-        //this is comping from Component class of react 
-        // setState form 1
-        // Calling setState we can trigger a re-render of our component with the updated values, react will do shallow merging(It means react will change qty in this.state) and after setState react will automatically re-render our component.
-        // this.setState({
-        //     qty: this.state.qty+1
-        // });
+//    increaseQuantity =() =>{
+//         // console.log("this.state", this.state);
+//         // this.state.qty+=1;
+//         //this is comping from Component class of react 
+//         // setState form 1
+//         // Calling setState we can trigger a re-render of our component with the updated values, react will do shallow merging(It means react will change qty in this.state) and after setState react will automatically re-render our component.
+//         // this.setState({
+//         //     qty: this.state.qty+1
+//         // });
 
-        // setState form 2, if we require the prevState (or values), we will use this function setState method otherwise the above object method.
-        this.setState((prevState)=>{
-            return {
-                qty: prevState.qty+1
-            }
-        }, ()=>{
-            // the secondcall back is for up to date data as setState() function is asynchronous we don't know when it will finish, so after clicking +1 to 2, qty in this.state will come 1 only.
-            console.log("this.state", this.state);
-        });
+//         // setState form 2, if we require the prevState (or values), we will use this function setState method otherwise the above object method.
+//         this.setState((prevState)=>{
+//             return {
+//                 qty: prevState.qty+1
+//             }
+//         }, ()=>{
+//             // the secondcall back is for up to date data as setState() function is asynchronous we don't know when it will finish, so after clicking +1 to 2, qty in this.state will come 1 only.
+//             console.log("this.state", this.state);
+//         });
          
-    }
+//     }
 
-    decreaseQuantity=()=>{
-        if(this.state.qty>0){
-        this.setState((prevState)=>{
-            return {qty: prevState.qty-1}
-        })
-    }
-    }
+//     decreaseQuantity=()=>{
+//         if(this.state.qty>0){
+//         this.setState((prevState)=>{
+//             return {qty: prevState.qty-1}
+//         })
+//     }
+//     }
 
-    deleteQuantity=()=>{
-
-    }
+   
 
     //Now even in promise also batching occurs (just for understanding) | No use of this function in code
     testing=()=>{
@@ -67,6 +65,10 @@ class CartItem extends React.Component{
         console.log("this.prop", this.props);
         //object destructuring
         const {price, title, qty, img} =this.props.product;
+        const increaseQuantity=this.props.onIncreaseQuantity;
+        const decreaseQuantity=this.props.onDecreaseQuantity;
+        const deleteQuantity=this.props.onDeleteQuantity;
+        const product=this.props.product;
         return(
             <>
                 <div className='cart-item'>
@@ -82,19 +84,19 @@ class CartItem extends React.Component{
                                 alt="increase" 
                                 className='action-icons' 
                                 src='https://img.icons8.com/?size=512&id=24717&format=png'
-                                onClick={this.increaseQuantity}
+                                onClick={()=>increaseQuantity(this.props.product)}  
                             />
                            <img 
                                 alt="decrease" 
                                 className='action-icons' 
                                 src='https://icon-library.com/images/decrease-icon/decrease-icon-8.jpg'
-                                onClick={this.decreaseQuantity}
+                                onClick={()=>decreaseQuantity(this.props.product)}
                             />
                            <img 
                                 alt="delete" 
                                 className='action-icons' 
                                 src='https://img.icons8.com/?size=512&id=3062&format=png'
-                                onClick={this.deleteQuantity}
+                                onClick={()=>deleteQuantity(this.props.product)}
                             />
                         </div>
                     </div>
